e177dd1cefe9a95c99d3ccbd96f17f0af4ade9e0
Tom Rose <tomrose@umich.edu>
2026-02-17 22:01:35 -0500

    spinner start

--- app/src/main/java/com/example/measurementconversionapplication_project1/activity_tomrosemm.kt
+++ app/src/main/java/com/example/measurementconversionapplication_project1/activity_tomrosemm.kt
@@ -1,16 +1,23 @@
 package com.example.measurementconversionapplication_project1
 
 import android.os.Bundle
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class activity_tomrosemm : AppCompatActivity() {
+
+    var conversions = arrayOf("Miles to Kilometers", "Kilometers to Miles", "Inches to Centimeters", "Centimeters to Inches")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tomrosemm)
+
+        val spinner = findViewById<Spinner>(R.id.conversionSpinner)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
