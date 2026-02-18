cc4d9d878fb313e8cde0b6be670fbec6cc9fbcb4
Tom Rose <tomrose@umich.edu>
2026-02-18 16:59:28 -0500

    spinner and layout tweaks

--- app/src/main/java/com/example/measurementconversionapplication_project1/MainActivity.kt
+++ app/src/main/java/com/example/measurementconversionapplication_project1/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.measurementconversionapplication_project1
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +14,15 @@ class MainActivity : AppCompatActivity() {
 
         val spinner = findViewById<Spinner>(R.id.conversionSpinner)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val adapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(
+            this,
+            R.array.conversion_options,
+            android.R.layout.simple_spinner_item
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner.adapter = adapter
+
     }
 }
