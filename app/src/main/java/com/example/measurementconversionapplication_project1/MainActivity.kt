b17d031f4313034b7649fd75ad8905ddf8002649
Alexander Afonso <158675700+alexafon@users.noreply.github.com>
2026-02-19 18:12:53 -0500

    Add input and result TextViews for conversion
    added the logic for conversions along with inputValue and resultText in the onCreate var

--- app/src/main/java/com/example/measurementconversionapplication_project1/MainActivity.kt
+++ app/src/main/java/com/example/measurementconversionapplication_project1/MainActivity.kt
@@ -18,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         val spinner = findViewById<Spinner>(R.id.conversionSpinner)
         val unitOneTextView = findViewById<TextView>(R.id.unitOneText)
         val unitTwoTextView = findViewById<TextView>(R.id.unitTwoText)
+        val inputValue = findViewById<EditText>(R.id.inputValue)
+        val resultText = findViewById<TextView>(R.id.resultText)
 
         val adapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(this, R.array.conversion_options, android.R.layout.simple_spinner_item)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -31,47 +33,61 @@ class MainActivity : AppCompatActivity() {
                 position: Int,
                 id: Long
             ) {
+
+                val input = inputValue.text.toString().toDoubleOrNull() ?: 0.0
+                var result = 0.0
+                
                 when (position) {
                     0 -> {
                         unitOneTextView.text = getString(R.string.miles)
                         unitTwoTextView.text = getString(R.string.kilometers)
+                        result = input * 1.6094
                     }
 
                     1 -> {
                         unitOneTextView.text = getString(R.string.kilometers)
                         unitTwoTextView.text = getString(R.string.miles)
+                        result = input / 1.6094
                     }
 
                     2 -> {
                         unitOneTextView.text = getString(R.string.inches)
                         unitTwoTextView.text = getString(R.string.centimeters)
+                        result = input * 2.54
                     }
 
                     3 -> {
                         unitOneTextView.text = getString(R.string.centimeters)
                         unitTwoTextView.text = getString(R.string.inches)
+                        result = input / 2.54
                     }
 
                     4 -> {
                         unitOneTextView.text = getString(R.string.teaspoons)
                         unitTwoTextView.text = getString(R.string.cups)
+                        result = input / 48
                     }
 
                     5 -> {
                         unitOneTextView.text = getString(R.string.cups)
                         unitTwoTextView.text = getString(R.string.teaspoons)
+                        result = input * 48
                     }
 
                     6 -> {
                         unitOneTextView.text = getString(R.string.ounces)
                         unitTwoTextView.text = getString(R.string.pounds)
+                        result = input / 16
                     }
 
                     7 -> {
                         unitOneTextView.text = getString(R.string.pounds)
                         unitTwoTextView.text = getString(R.string.ounces)
+                        result = input * 16
                     }
                 }
+
+                resultText.text = String.format("%.4f", result)
             }
 
             override fun onNothingSelected(parent: AdapterView<*>) {
